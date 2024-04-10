727065494fcec52bc7ff6dd4562bce3ecb152c30
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-04-10 08:27:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,11 +4,45 @@
 #include "tstack.h"
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+    std::string post;
+    TStack<char, 100> stack;
+    
+    for (char &c : inf) {
+        if (c >= '0' && c <= '9') {
+            post += c;
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            while (!stack.isEmpty() && ((stack.peek() == '*' || stack.peek() == '/') && (c == '+' || c == '-'))) {
+                post += stack.pop();
+            }
+            stack.push(c);
+        }
+    }
+    while (!stack.isEmpty()) {
+        post += stack.pop();
+    }
+    return post;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+TStack<int, 100> stack;
+    
+    for (char &c : post) {
+        if (c >= '0' && c <= '9') {
+            stack.push(c - '0');
+        } else {
+            int operand2 = stack.pop();
+            int operand1 = stack.pop();
+            
+            if (c == '+') {
+                stack.push(operand1 + operand2);
+            } else if (c == '-') {
+                stack.push(operand1 - operand2);
+            } else if (c == '*') {
+                stack.push(operand1 * operand2);
+            } else if (c == '/') {
+                stack.push(operand1 / operand2);
+            }
+        }
+    }
+    return stack.pop();
 }
