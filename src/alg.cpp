fa10ebb7e44a5930f67e89b61067eb4fb38a6b49
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-04-10 08:44:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,13 +6,14 @@
 std::string infx2pstfx(std::string inf) {
     std::string post;
     TStack<char, 100> stack;
-    
     for (char &c : inf) {
         if (c >= '0' && c <= '9') {
             post += c;
         } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-            while (!stack.isEmpty() && ((stack.peek() == '*' || stack.peek() == '/') && (c == '+' || c == '-'))) {
-                post += stack.pop();
+            while (!stack.isEmpty() && (stack.peek() == ' ' || stack.peek() == '/')) {
+                while (!stack.isEmpty() && (c == '+' || c == '-')) {
+                    post += stack.pop();
+                }
             }
             stack.push(c);
         }
@@ -25,14 +26,12 @@ std::string infx2pstfx(std::string inf) {
 
 int eval(std::string pref) {
 TStack<int, 100> stack;
-    
     for (char &c : post) {
         if (c >= '0' && c <= '9') {
             stack.push(c - '0');
         } else {
             int operand2 = stack.pop();
             int operand1 = stack.pop();
-            
             if (c == '+') {
                 stack.push(operand1 + operand2);
             } else if (c == '-') {
