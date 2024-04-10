8ebe58f6e8e6d945e3f56bdd9238ff4ccc1d634a
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-04-10 10:25:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,45 +3,91 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-    std::string post;
-    TStack<char, 100> stack;
-    for (char &c : inf) {
-        if (c >= '0' && c <= '9') {
-            post += c;
-        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-            while (!stack.isEmpty() &&
-                ((stack.peek() == '*' || stack.peek() == '/') &&
-                (c == '+' || c == '-'))) {
-                post += stack.pop();
-            }
-            stack.push(c);
-        }
-    }
-    while (!stack.isEmpty()) {
-        post += stack.pop();
-    }
-    return post;
+bool operation(char iChar) {
+    return (iChar == '*' || iChar == '/' || iChar == '-' || iChar == '+');
 }
-
+int priority(char iOper) {
+    if (iOper == '*' || iOper == '/') return 2;
+    if (iOper == '+' || iOper == '-') return 1;
+    return 0;
+}
+std::string infx2pstfx(std::string inf) {
+    std::string postf;
+    TStack<char, 100> stack1;
+    for (int i = 0; i < inf.size(); i++) {
+        char iChar = inf[i];
+        if (isdigit(iChar)) {
+            int operand = 0;
+            while (i < inf.size() && isdigit(inf[i])) {
+                operand = operand * 10 + (inf[i] - '0');
+                i++;
+            }
+            i--;
+            postf += std::to_string(operand) + " ";
+        }
+        else if (isalpha(iChar)) {
+            postf += iChar;
+            postf += " ";
+        }
+        else if (iChar == '(') {
+            stack1.push(iChar);
+        }
+        else if (iChar == ')') {
+            while (!stack1.isEmpty() && stack1.watch() != '(') {
+                postf += stack1.pop();
+                postf += ' ';
+            }
+            if (!stack1.isEmpty() && stack1.watch() == '(') {
+                stack1.pop();
+            }
+        }
+        else if (operation(iChar)) {
+            while (!stack1.isEmpty() &&
+                priority(stack1.watch()) >= priority(iChar)) {
+                postf += stack1.pop();
+                postf += ' ';
+            }
+            stack1.push(iChar);
+        }
+    }
+    while (!stack1.isEmpty()) {
+        postf += stack1.pop();
+        postf += ' ';
+    }
+    if (!postf.empty()) {
+        postf.pop_back();
+    }
+    return postf;
+}
+ 
 int eval(std::string post) {
-TStack<int, 100> stack;
-    for (char &c : post) {
-        if (c >= '0' && c <= '9') {
-            stack.push(c - '0');
-        } else {
-            int operand2 = stack.pop();
-            int operand1 = stack.pop();
-            if (c == '+') {
-                stack.push(operand1 + operand2);
-            } else if (c == '-') {
-                stack.push(operand1 - operand2);
-            } else if (c == '*') {
-                stack.push(operand1 * operand2);
-            } else if (c == '/') {
-                stack.push(operand1 / operand2);
+    TStack<int, 100> stack2;
+    for (int i = 0; i < pref.size(); i++) {
+        if (isdigit(pref[i])) {
+            int operand = 0;
+            while (i < pref.size() && isdigit(pref[i])) {
+                operand = operand * 10 + (pref[i] - '0');
+                i++;
+            }
+            i--;
+            stack2.push(operand);
+        }
+        else if (operation(pref[i])) {
+            int iOper2 = stack2.pop();
+            int iOper1 = stack2.pop();
+            if (pref[i] == '+') {
+                stack2.push(iOper1 + iOper2);
+            }
+            else if (pref[i] == '-') {
+                stack2.push(iOper1 - iOper2);
+            }
+            else if (pref[i] == '*') {
+                stack2.push(iOper1 * iOper2);
+            }
+            else if (pref[i] == '/') {
+                stack2.push(iOper1 / iOper2);
             }
         }
     }
-    return stack.pop();
+    return stack2.pop();
 }
